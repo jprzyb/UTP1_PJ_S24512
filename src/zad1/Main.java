ebf1a3f9c807b2598a23ef921c58bab4368a8599
Jakub Przybylski <jakubp02.10@gmail.com>
2024-03-26 20:48:43 +0100

    Working code. Finished.

--- src/zad1/Main.java
+++ src/zad1/Main.java
@@ -29,6 +29,8 @@ public class Main {
     XList<Integer> list5 = new XList(set);
     XList<Integer> list6 = XList.of(set);
 
+
+    System.out.println("\n======================INIT======================");
     System.out.println(list1);
     System.out.println(list2);
     System.out.println(list3);
@@ -41,11 +43,13 @@ public class Main {
     XList<String> slist2 = XList.tokensOf("ala ma kota");
     XList<String> slist3 = XList.tokensOf("A-B-C", "-");
 
+    System.out.println("\n======================Helping methods======================");
     System.out.println(slist1);
     System.out.println(slist2);
     System.out.println(slist3);
 
-    // Metoda union - suma elementów 
+    // Metoda union - suma elementów
+    System.out.println("\n======================Union1======================");
     List<Integer> m1 = list1.union(list2);  // oczywiście, można podstawiać na List
     System.out.println(m1);
     // można wykonywać wszystkie operacje z interfejsu List, np:
@@ -53,6 +57,7 @@ public class Main {
     System.out.println(m1);
     XList<Integer> m2 = (XList<Integer>) m1;
     XList<Integer> m3 = m2.union(ints).union(XList.of(4, 4));
+    System.out.println("\n======================Union2======================");
     System.out.println(m2); // m2 się nie zmienia
     System.out.println(m3); // wynik jest w m3
     m3 = m3.union(set);
@@ -60,11 +65,13 @@ public class Main {
 
     // Widzieliśmy metode union
     // Teraz metoda diff(dowolna kolekcja)
+    System.out.println("\n======================Diff======================");
     System.out.println(m3.diff(set));  // wszystko z m3, co nie jest w set
     System.out.println(XList.of(set).diff(m3)); // co jest w set, czego nie ma w m3
 
-    // Metoda unique -zwraca nową Xlist bez duplikatow   
+    // Metoda unique -zwraca nową Xlist bez duplikatow
     XList<Integer> uniq = m3.unique(); // lista, nie Set
+    System.out.println("\n======================Unique======================");
     System.out.println(uniq);
 
     // kombinacje (kolejność jest istotna)
@@ -72,24 +79,27 @@ public class Main {
     List<String> sb = Arrays.asList( "X", "Y", "Z" );
     XList<String> sc = XList.charsOf( "12" );
     XList toCombine = XList.of(sa, sb, sc);
+    System.out.println("\n======================Combinations======================");
     System.out.println(toCombine);
     XList<XList<String>> cres = toCombine.combine();
     System.out.println(cres);
 
     // collect i join
+    System.out.println("\n======================Collect&Join======================");
     XList<String> j1 = cres.collect( list -> list.join());
     System.out.println(j1.join(" "));
     XList<String> j2 =cres.collect( list -> list.join("-"));
     System.out.println(j2.join(" "));
 
     // forEachWithIndex
-    XList<Integer> lmod = XList.of(1,2,8, 10, 11, 30, 3, 4);  
+    System.out.println("\n======================ForEachWithIndex======================");
+    XList<Integer> lmod = XList.of(1,2,8, 10, 11, 30, 3, 4);
     lmod.forEachWithIndex( (e, i) -> lmod.set(i, e*2));
     System.out.println(lmod);
     lmod.forEachWithIndex( (e, i) -> { if (i % 2 == 0) lmod.remove(e); } );
     System.out.println(lmod);
     lmod.forEachWithIndex( (e, i) -> { if (i % 2 == 0) lmod.remove(i); } );
-    System.out.println(lmod); // Pytanie: dlaczego mamy taki efekt? 
+    System.out.println(lmod); // Pytanie: dlaczego mamy taki efekt?
 
   }
 }
